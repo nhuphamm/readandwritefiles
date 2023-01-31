7513d4f1986cbf341fa38e18a3b72c86c6e34821
NhuPham <you@example.com>
2023-01-31 02:24:39 -0600

    file

--- customer_country.py
+++ customer_country.py
@@ -1,15 +1,25 @@
 import csv
+import pandas as pd
 
-with open("customers.csv", "r") as csvfile:
-    csvreader = csv.DictReader(csvfile)
+# with open("customers.csv", "r") as csvfile:
+#     csvreader = csv.DictReader(csvfile)
 
-    with open("customer_country.csv", "w") as newfile:
-        fieldnames = ["FirstName", "LastName", "City"]
-        csv_writer = csv.DictWriter(newfile, fieldnames=fieldnames)
-        csv_writer.writeheader()
+#     with open("customer_country.csv", "w") as newfile:
+#         fieldnames = ["FirstName", "LastName", "City"]
+#         csv_writer = csv.DictWriter(newfile, fieldnames=fieldnames)
+#         csv_writer.writeheader()
 
-        for line in csvreader:
-            del line["\ufeffID"]
-            del line["Country"]
-            del line["Phone"]
-            csv_writer.writerow(line)
+#         for line in csvreader:
+#             del line["\ufeffID"]
+#             del line["Country"]
+#             del line["Phone"]
+#             csv_writer.writerow(line)
+
+csv_input = pd.read_csv("customers.csv", delimiter=",")
+csv_input["FullName"] = csv_input["FirstName"].astype(str) + " " + csv_input["LastName"]
+
+print(f"Total number of customer: {len(csv_input)} customers")
+
+output = csv_input[["FullName", "Country"]]
+
+output.to_csv("customer_country.csv", index=False)
--- employee_bonus.py
+++ employee_bonus.py
@@ -1,11 +1,20 @@
 import csv
 
 with open("EmployeePay.csv", "r") as file:
-    csv_reader = csv.DictReader(file)
+    csv_reader = csv.DictReader(file, delimiter=",")
 
-    for row in csv_reader:
-        print(row)
+    for record in csv_reader:
 
-    # pause = input("Press Enter to continue")
-    # if pause == "Enter":
-    #     break
+        record2 = record["EmpFName"]
+        record3 = record["EmpLName"]
+        record4 = float(record["Salary"])
+        record5 = float(record["Bonus"])
+
+        total = (record4 * record5) + record4
+
+        print("EmpFName: ", record2)
+        print("EmpLName: ", record3)
+        print("Salary: ", record4)
+        print("Bonus: ", record5)
+        print("Total: ", total)
+        input("Press Enter to continue")
